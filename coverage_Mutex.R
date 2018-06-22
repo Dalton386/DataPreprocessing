aaa69b4a270dc77a50060a54d0be7ad03651fe8f
DujianDing <dujiand@sfu.ca>
2018-06-22 14:58:47 -0700

    Update: support repeated queries for different thresholds;

--- coverage_Mutex.R
+++ coverage_Mutex.R
@@ -3,7 +3,6 @@ filename = readline("matrix file: ")
 conname = readline("ranked-groups file: ")
 
 rawdata = read.table(paste0('./data/', filename), as.is = TRUE)
-con = file(paste0("./data/", conname), "r")
 
 rownum = dim(rawdata)[1]
 colnum = dim(rawdata)[2]
@@ -13,28 +12,33 @@ rownames(mutation) = rawdata[2:rownum,1]
 colnames(mutation) = rawdata[1,2:colnum]
 mutation = as.table(mutation)
 
-threshold = as.numeric(readline("coverage threshold: "))
+threshold = as.numeric(readline("coverage threshold(>0): "))
 
-line = readLines(con, n = 1)
-coverage = matrix(0L, nrow = 1, ncol = colnum-1)
-visited = c()
-count = 1
-while (TRUE) {
+while (threshold > 0){
+  con = file(paste0("./data/", conname), "r")
   line = readLines(con, n = 1)
-  ratio = sum(coverage) / length(coverage)
-  if (length(line) == 0 || ratio >= threshold)
-    break
-  count = count + 1
-  fields = strsplit(line, '\t')[[1]]
-  
-  for (i in 2:length(fields)){
-    if (!(fields[i] %in% visited)){
-      visited = c(vector, fields[i])
-      coverage = 1 * (coverage | mutation[fields[i], ])
+  coverage = matrix(0L, nrow = 1, ncol = colnum-1)
+  visited = c()
+  count = 1
+  while (TRUE) {
+    line = readLines(con, n = 1)
+    ratio = sum(coverage) / length(coverage)
+    if (length(line) == 0 || ratio >= threshold)
+      break
+    count = count + 1
+    fields = strsplit(line, '\t')[[1]]
+    
+    for (i in 2:length(fields)){
+      if (!(fields[i] %in% visited)){
+        visited = c(vector, fields[i])
+        coverage = 1 * (coverage | mutation[fields[i], ])
+      }
     }
+    
   }
-  
+  print(paste("the coverage is", ratio))
+  print(paste("pathways are above(included) line", count))
+  threshold = as.numeric(readline("coverage threshold(>0): "))
 }
-print(paste("the coverage is ", ratio))
-print(paste("the last read line is ", count))
+
 close(con)
